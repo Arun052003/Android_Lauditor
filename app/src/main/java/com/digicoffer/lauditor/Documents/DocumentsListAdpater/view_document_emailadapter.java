a9e18b1a0b6997191aa200349a542949ec430b0d
shobika17 <223172097@sastra.ac.in>
2024-04-24 17:16:58 +0530

    updating adapter .java

--- app/src/main/java/com/digicoffer/lauditor/Documents/DocumentsListAdpater/view_document_emailadapter.java
+++ app/src/main/java/com/digicoffer/lauditor/Documents/DocumentsListAdpater/view_document_emailadapter.java
@@ -2,10 +2,12 @@ package com.digicoffer.lauditor.Documents.DocumentsListAdpater;
 
 import android.content.Context;
 import android.graphics.Paint;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.Filter;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -13,9 +15,13 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.digicoffer.lauditor.Documents.models.DocumentsModel;
 import com.digicoffer.lauditor.Documents.models.ViewDocumentsModel;
 import com.digicoffer.lauditor.R;
 import com.digicoffer.lauditor.common.AndroidUtils;
+import com.digicoffer.lauditor.common.Constants;
+
+import org.json.JSONArray;
 
 import java.util.ArrayList;
 
@@ -23,6 +29,7 @@ public class view_document_emailadapter extends RecyclerView.Adapter<view_docume
     //ArrayList<ViewDocumentsModel> docsList = new ArrayList<>();
     ArrayList<ViewDocumentsModel> list_item;
     ArrayList<ViewDocumentsModel> itemsArrayList;
+    ArrayList<ViewDocumentsModel> docs_list=new ArrayList<>();
     private ArrayList<String> selectedDocumentNames = new ArrayList<>();
     private ArrayList<String> selectedDocumentIds = new ArrayList<>();
     private ArrayList<String> selectedDocumentPaths = new ArrayList<>();
@@ -85,19 +92,37 @@ public class view_document_emailadapter extends RecyclerView.Adapter<view_docume
             holder.tv_doc_description.setText(viewDocumentsModel.getDescription());
 //            holder.tv_document_display_name.setText(viewDocumentsModel.getName());
             holder.tv_image_name.setText(viewDocumentsModel.getName());
-            holder.checkbox_id.setOnCheckedChangeListener((buttonView, isChecked) -> {
-                if (isChecked) {
-                    // Checkbox is checked, add document details to selected lists
-                    selectedDocumentNames.add(viewDocumentsModel.getName());
-                    selectedDocumentIds.add(viewDocumentsModel.getId());
+//           holder.checkbox_id.setText(viewDocumentsModel.getId());
+            // Assuming checkbox_id is a CheckBox variable declared at the class level
+           holder. checkbox_id.setTag(viewDocumentsModel);
 
-                } else {
-                    // Checkbox is unchecked, remove document details from selected lists
-                    selectedDocumentNames.remove(viewDocumentsModel.getName());
-                    selectedDocumentIds.remove(viewDocumentsModel.getId());
+            holder.  checkbox_id.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if (isChecked) {
+                        ViewDocumentsModel documentModel = (ViewDocumentsModel) buttonView.getTag();
+                        docs_list.add(documentModel);
+                        if (documentModel != null) {
+                            JSONArray docs = new JSONArray();
+                            for (int k = 0; k < docs_list.size(); k++) {
+                                ViewDocumentsModel documentsModel1 = docs_list.get(k);
+                                docs.put(documentsModel1.getId());
+                                String documentId=documentModel.getId();
+                                String documentName = documentModel.getName();
 
+                                Log.d("DocumentInfo", "ID: " + documentId + ", Name: " + documentName);
+                            }
+                            try {
+                                Constants.doc_id=docs;
+                            }catch (Exception e)
+                            {
+                                e.fillInStackTrace();
+                            }
+                        }
+                    }
                 }
             });
+
 //            Log.d("IMage_name", viewDocumentsModel.getName());
             holder.tv_created_date.setText(viewDocumentsModel.getCreated());
             holder.tv_Expiration_date.setText(viewDocumentsModel.getExpiration_date());
@@ -119,25 +144,24 @@ public class view_document_emailadapter extends RecyclerView.Adapter<view_docume
 
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
-        com.google.android.material.imageview.ShapeableImageView siv_profile_icon;
-        ImageView iv_doc_image, iv_edit_document, uv_delete_document, iv_view_document;
+
+
         CheckBox checkbox_id;
+
+        ViewDocumentsModel viewDocumentsModel;
         TextView tv_document_display_name, tv_Expiration, tv_client_name_one, tv_image_name, tv_Expiration_date, tv_client_name, tv_doc_description, tv_created_date;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
-            //  siv_profile_icon = itemView.findViewById(R.id.iv_display_picture);
-//            iv_doc_image = itemView.findViewById(R.id.iv_doc_image);
-//            uv_delete_document = itemView.findViewById(R.id.uv_delete_document);
-//            iv_edit_document = itemView.findViewById(R.id.iv_edit_document);
-//            tv_document_display_name = itemView.findViewById(R.id.tv_document_display_name);
-//            tv_client_name_one = itemView.findViewById(R.id.tv_client_name_one);
-//            tv_client_name_one.setTextSize(12);
+
             tv_image_name = itemView.findViewById(R.id.tv_image_name);
             tv_client_name = itemView.findViewById(R.id.tv_client_name);
             tv_client_name.setTextSize(15);
             checkbox_id = itemView.findViewById(R.id.checkbox_id);
             checkbox_id.setVisibility(View.VISIBLE);
+
+
+
 
 
             tv_doc_description = itemView.findViewById(R.id.tv_doc_description);
@@ -148,7 +172,7 @@ public class view_document_emailadapter extends RecyclerView.Adapter<view_docume
             tv_Expiration.setText("Expiration:");
             tv_Expiration_date = itemView.findViewById(R.id.tv_Expiration_date);
             tv_Expiration_date.setText("Expiration");
-//            iv_view_document = itemView.findViewById(R.id.iv_edit_view);
+
         }
     }
 }
